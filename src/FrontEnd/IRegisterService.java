16789215d0a73f20d933bea989623f7f15d2ee1f
CrowlLiu <3041810152@qq.com>
2020-03-15 20:09:20 +0800

    final

---
+++ src/FrontEnd/IRegisterService.java
@@ -0,0 +1,11 @@
+public interface IRegisterService
+{
+    /*** 测试用接口，注意替换。将用户预约登记的信息提交
+     * @param userName 用户真实姓名
+     * @param userIdentify 用户身份证号
+     * @param userPhone 用户手机号
+     * @param num 用户预约的口罩数量
+     */
+     int sendMessage(String userName,String userIdentify,String userPhone,int num);
+
+}
