671c6cd73ddf49c62a9ef4da837f1db0a8926f68
Chenqiyuan <627064471@qq.com>
2020-03-15 15:55:37 +0800

    删除错误的Appointment相关类

--- src/dao/Appointment.java
+++ src/dao/Appointment.java
@@ -3,13 +3,7 @@ package dao;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-/**
- * @ClassName Appointment
- * @Description TODO
- * @Author mingll
- * @Date 2020/3/15 2:01 下午
- * @Version 1.0
- */
+
 public interface Appointment {
     /**
      * 新建一次预约项目
